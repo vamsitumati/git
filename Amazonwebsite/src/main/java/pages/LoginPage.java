3eff1c11f34f525501551f2985bbbd788f996785
KISH <KISH@172.16.57.233>
2016-11-22 13:05:56 -0500

    just kishan committed

--- Amazonwebsite/src/main/java/pages/LoginPage.java
+++ Amazonwebsite/src/main/java/pages/LoginPage.java
@@ -1,6 +1,8 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 
 public class LoginPage extends WebDriverFactory {
@@ -12,5 +14,16 @@ public class LoginPage extends WebDriverFactory {
 		  getDriver().findElement(By.xpath(".//input[@id='ap_password'][@name='password']")).sendKeys("654321");
 		  getDriver().findElement(By.xpath(".//input[@id='signInSubmit'][@type='submit']"));
 	  }
-
+	  public void Log() throws InterruptedException{
+		  Actions action = new Actions(getDriver());
+		  WebElement we = getDriver().findElement(By.xpath(".//*[@id='nav-link-accountList']"));
+		  action.moveToElement(we).build().perform();
+		  Thread.sleep(4000);
+		  getDriver().findElement(By.xpath(".//*[@id='nav-flyout-ya-signin']/a/span")).click();
+		  Thread.sleep(4000);
+      	  getDriver().findElement(By.cssSelector("#ap_email")).sendKeys("kishantej");
+		  getDriver().findElement(By.xpath(".//input[@id='ap_password'][@name='password']")).sendKeys("8164054060");
+		  getDriver().findElement(By.xpath(".//input[@id='signInSubmit'][@type='submit']"));
+		
+	  }
 }
--- Amazonwebsite/src/main/java/tests/VerifyPage.java
+++ Amazonwebsite/src/main/java/tests/VerifyPage.java
@@ -6,12 +6,20 @@ import pages.LoginPage;
 
 public class VerifyPage {
 	//Vamsi
+//			@Test
+//			public void signinVerification() throws InterruptedException{
+//				LoginPage signin = new LoginPage();
+//				signin.intializeWebDriver();
+//				signin.SignIn();
+//				signin.tearDown();
+//				
+//			}
+			//kishan
 			@Test
-			public void signinVerification() throws InterruptedException{
-				LoginPage signin = new LoginPage();
-				signin.intializeWebDriver();
-				signin.SignIn();
-				signin.tearDown();
-				
+			public void LoginVerification() throws InterruptedException{
+				LoginPage Login = new LoginPage();
+				Login.intializeWebDriver();
+				Login.Log();
+				Login.tearDown();
 			}
 }
